05200aaecd4a24b3d3d81c1392c3898c7499acc9
GabrielDamian <damiangabriel54@yahoo.com>
2021-08-15 16:43:56 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 const Todo = require('./models/Todo');
 const Day = require('./models/Day');
 
